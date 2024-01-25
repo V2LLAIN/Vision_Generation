37de3d7c6397a2fa5c7d5118b7176266fc37d0d6
V2LLAIN <104286511+V2LLAIN@users.noreply.github.com>
2024-01-25 16:14:13 +0900

    Create initialize_weights.py

---
+++ Implicit_Density/DCGAN/initialize_weights.py
@@ -0,0 +1,7 @@
+def initialize_weights(w):
+    classname = w.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(w.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(w.weight.data, 1.0, 0.02)
+        nn.init.constant_(w.bias.data, 0)
