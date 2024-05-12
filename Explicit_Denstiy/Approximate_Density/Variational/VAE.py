10501313f6a50184eddce1e2374eec29917e4ff6
V2LLAIN <104286511+V2LLAIN@users.noreply.github.com>
2024-05-12 21:03:35 +0900

    Update VAE.py

--- Explicit_Denstiy/Approximate_Density/Variational/VAE.py
+++ Explicit_Denstiy/Approximate_Density/Variational/VAE.py
@@ -1,33 +1,48 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.Functional as F
+from torch.autograd import Variable
 
 class VAE(nn.Module):
-    def __init__(self):
-        super (VAE, self).__init_()
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear (400, 20)
-        self.fc22 = nn.Linear (400, 20)
-        self.fc3 = nn.Linear (20, 400)
-        self. fc4 = nn.Linear (400, 784)
+    def __init__(self, input_dim=784, hidden_dim=256, latent_dim=64):
+        super(VAE, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+        )
+        self.mu = nn.Linear(hidden_dim, latent_dim)
+        self.logvar = nn.Linear(hidden_dim, latent_dim)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hiddens_dim, input_dim),
+            nn.Sigmoid(),
+        )
+    
     def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        hidden = self.encoder(x)
+        mu = self.mu(hidden)
+        logvar = self.logvar(hidden)
+        return mu, logvar
+
     def reparameterize(self, mu, logvar):
-        if self.training:
-            std = torch.exp(0.5*logvar)
-            eps = torch.randn_like(std)
-            return eps.mul(std).add_(mu)
-        else:
-            return mu
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        z = mu + eps*std
+        return z
+
     def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return F.sigmoid(self.fc4(h3))
+        output = self.decoder(z)
+        return output
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
+        mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        output = self.decode(z)
+        return output, mu, logvar
 
     # Reconstruction + KL divergence losses summed over all elements and batch
     def loss_function (recon_x, x, mu, logvar):
